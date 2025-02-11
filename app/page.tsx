ce6eb04d54dbc3c2374d15b0283baf7f1260f6ee
Elkana Kipruto <allanyegon443@gmail.com>
2025-02-11 17:41:43 +0300

    Added inset

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-main via-secondary to-main">
+      <div className="container w-auto rounded-md inset-0 bg-black">
       <ParticlesComponent/>
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
@@ -23,11 +24,11 @@ export default function Home() {
             </Link>
           ))}
         </ul>
+        <div className="hidden items-center h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />   
+          <p className="text-center p-3 text-thro text-lg">I am Yegon Elkana, <span className="animate-glow text-xl drop-shadow-md animate-pulse">{`</Welcome>`}</span> to my portfolio</p>
+        <div/>
       </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-        
-        <p className="text-thro text-lg">I am Yegon Elkana, <span className="animate-glow text-xl drop-shadow-md animate-pulse">{`</Welcome>`}</span> to my portfolio</p>
-      <div/>
+      </div>
     </div>
   );
 }
